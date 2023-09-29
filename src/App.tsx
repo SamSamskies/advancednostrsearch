bea51a41d4e2afd9fa6712968cc91a5236c0ca81
Daniele Tonon <tonon@vitamino.it>
2023-09-29 17:31:01 +0200

    Switch to njump.me

--- src/App.tsx
+++ src/App.tsx
@@ -289,7 +289,7 @@ export default function App() {
                 </Text>
                 <NoteContent content={content} />
                 <HStack mt={4} justifyContent="right">
-                  <Link href={`https://nostr.com/${noteId}`} isExternal>
+                  <Link href={`https://njump.me/${noteId}`} isExternal>
                     <Button>Open</Button>
                   </Link>
                   <Button
